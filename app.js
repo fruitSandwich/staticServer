5a23e92cc69fc0b6e5cab8eb0a599273f2335824
Jiaheng Du <dujiaheng@CZ1CG1.PRI>
2018-09-06 14:41:18 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Created by dujiaheng on 2017/6/20.
+ */
+
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use("/", express.static(__dirname + '/static'));
+app.use("/health",function(req,res){
+	res.send({"status":"UP"})
+})
+// 创建服务端
+http.createServer(app).listen('8080', function () {
+    console.log('service is running...');
+});
